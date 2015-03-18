69c624f3629b6b518741776b7b3cc2d44c6fc2b8
Greg Holguin <simulatedgreg@gmail.com>
2015-03-17 23:17:59 -0500

    cache api on each session

--- kappa.js
+++ kappa.js
@@ -1,28 +1,47 @@
 (function($) {
 	$.fn.kappa = function () {
-		var target = $(this);
 
 		// Init global variables
+		var target = $(this);
 		var API, EMOTES;
 		var TEMPLATE_SMALL, TEMPLATE_MEDIUM, TEMPLATE_LARGE;
-
-		// GET list of Global Emotes from Twitch Emotes API v2
-		$.ajax({
-			url: 'http://twitchemotes.com/api_cache/v2/global.json',
-			success: function(data) {
-				console.log(data);
-				define(data);
-			},
-			error: function() {
-				alert('Connection to Twitch Emotes API v2 has failed.')
-			}
-		});
+		
+		// Check for local storage support
+		if(!(typeof(Storage) !== "undefined")) {
+		   	if(sessionStorage.twitchemotesAPI==null) {
+		   		// GET list of Global Emotes from Twitch Emotes API v2
+				$.ajax({
+					url: 'http://twitchemotes.com/api_cache/v2/global.json',
+					success: function(data) {
+						sessionStorage.twitchemotesAPI = JSON.stringify(data);
+						define(sessionStorage.twitchemotesAPI);
+					},
+					error: function() {
+						alert('Connection to Twitch Emotes API v2 has failed.')
+					}
+				});
+		   	} else {
+		   		define(sessionStorage.twitchemotesAPI);
+		   	}
+		} else {
+		    // GET list of Global Emotes from Twitch Emotes API v2
+			$.ajax({
+				url: 'http://twitchemotes.com/api_cache/v2/global.json',
+				success: function(data) {
+					data = JSON.stringify(data);
+					define(data);
+				},
+				error: function() {
+					alert('Connection to Twitch Emotes API v2 has failed.')
+				}
+			});
+		}
 
 		// Define global variables Urls
 		function define(data) {
-			API = data;
-			EMOTES = data.emotes;
+			API = JSON.parse(data);
 
+			EMOTES = API.emotes;
 			TEMPLATE_SMALL = API.template.small;
 			TEMPLATE_MEDIUM = API.template.medium;
 			TEMPLATE_LARGE = API.template.large;
